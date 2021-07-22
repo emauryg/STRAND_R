100db7ff56b89855f07597e1b8f6055a904f0bb7
emauryg <emauryg1@gmail.com>
2021-07-21 23:00:55 -0400

    updated theta

--- R/theta_estimate.R
+++ R/theta_estimate.R
@@ -31,7 +31,7 @@ estimate_theta <- torch::nn_module(
           diff1 = self$eta[,b, drop=FALSE] - mu[,b, drop=FALSE]
           fun = torch_diag(-0.5*diff1$transpose(1,2)$matmul(SigmaInv)$matmul(diff1))
           fun1 = fun + torch_diag(yphi_[,,,,,b,,]$matmul(torch_log(theta+1e-14))$sum(dim=c(1,2,3,4,5,7)))
-          fun2 = fun2 -fun1$mean()
+          fun2 = fun2 -fun1
         }
       }
     } else {
@@ -41,7 +41,7 @@ estimate_theta <- torch::nn_module(
       fun1 = fun + torch_diag(yphi_$matmul(torch_log(theta + 1e-14))$sum(dim=c(1,2,3,4,5,7)))
       fun2 = -fun1$mean()
     }
-    return(fun2)
+    return(fun2/D)
   } 
 )
 
