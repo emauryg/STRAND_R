4789d5ed904484c746801bbc9011a41e145bee32
emauryg <emauryg1@gmail.com>
2021-08-05 06:12:04 -0400

    removed torch_square from covariance regularization

--- R/optimize_tnf.R
+++ R/optimize_tnf.R
@@ -59,45 +59,27 @@ tnf <- torch::nn_module(
         pred = T_tensor$matmul(torch_diag_embed(F_tensor))
         loss_val =  -(yphi_tensor$sum(dim=-3)*torch_log(pred + 1e-14))$sum()/(D*K)
         weight =  tau
-        #reg = covariance_regularizer(t0_, r0_,e0_, n0_, c0_, self$factor_dim)
         Cr = torch_mm(self$r$transpose(1,2), self$r) / 2
+        Ct = torch_mm(self$t$transpose(1,2), self$t) / 2        
         Ce = torch_mm(self$e$transpose(1,2), self$e) / factor_dim[3]
-        Ct = torch_mm(self$t$transpose(1,2), self$t) / 2
         Cn = torch_mm(self$n$transpose(1,2), self$n) / factor_dim[4]
         Cc = torch_mm(self$c$transpose(1,2), self$c) / factor_dim[5]
 
-        reg = torch_square( Ct - torch_diag(torch_diag(Ct)))$sum()/2 + 
-                torch_square(Cr - torch_diag(torch_diag(Cr)))$sum()/2 +
-                torch_square(Ce - torch_diag(torch_diag(Ce)))$sum()/factor_dim[3] +
-                torch_square(Cn - torch_diag(torch_diag(Cn)))$sum()/factor_dim[4] +
-                torch_square(Cc - torch_diag(torch_diag(Cc)))$sum()/factor_dim[5]
+        # reg = torch_square( Ct - torch_diag(torch_diag(Ct)))$sum()/2 + 
+        #         torch_square(Cr - torch_diag(torch_diag(Cr)))$sum()/2 +
+        #         torch_square(Ce - torch_diag(torch_diag(Ce)))$sum()/factor_dim[3] +
+        #         torch_square(Cn - torch_diag(torch_diag(Cn)))$sum()/factor_dim[4] +
+        #         torch_square(Cc - torch_diag(torch_diag(Cc)))$sum()/factor_dim[5]
+        reg = ( Ct - torch_diag(torch_diag(Ct)))$sum()/2 + 
+            (Cr - torch_diag(torch_diag(Cr)))$sum()/2 +
+            (Ce - torch_diag(torch_diag(Ce)))$sum()/factor_dim[3] +
+            (Cn - torch_diag(torch_diag(Cn)))$sum()/factor_dim[4] +
+            (Cc - torch_diag(torch_diag(Cc)))$sum()/factor_dim[5]
 
         return( loss_val + weight*reg)
     }
 )
 
-covariance_regularizer <- function(t0_, r0_,e0_, n0_, c0_,factor_dim){
-    t = nnf_softmax(t0_, dim=1)
-    r = nnf_softmax(r0_,dim=1)
-    e = nnf_softmax(e0_, dim=1)
-    n = nnf_softmax(n0_, dim=1)
-    c = nnf_softmax(c0_, dim=1)
-
-    Ct = torch_mm(t$transpose(1,2), t) / 2
-    Cr = torch_mm(r$transpose(1,2), r) / 2
-    Ce = torch_mm(e$transpose(1,2), e) / factor_dim[3]
-    Cn = torch_mm(n$transpose(1,2), n) / factor_dim[4]
-    Cc = torch_mm(c$transpose(1,2), c) / factor_dim[5]
-
-    covariance_penalty = torch_square( Ct - torch_diag(torch_diag(Ct)))$sum()/2 + 
-                    torch_square(Cr - torch_diag(torch_diag(Cr)))$sum()/2 +
-                    torch_square(Ce - torch_diag(torch_diag(Ce)))$sum()/factor_dim[3] +
-                    torch_square(Cn - torch_diag(torch_diag(Cn)))$sum()/factor_dim[4] +
-                    torch_square(Cc - torch_diag(torch_diag(Cc)))$sum()/factor_dim[5]
-
-    return(covariance_penalty)
-
-}
 
 
 
