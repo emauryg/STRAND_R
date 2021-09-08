e1284f6c32d364b6de6b4ca027993c35b5302bfb
emauryg <emauryg1@gmail.com>
2021-09-08 06:12:05 -0400

    back to the same

--- R/theta_estimate.R
+++ R/theta_estimate.R
@@ -9,26 +9,37 @@ estimate_theta <- torch::nn_module(
     #self$mu = mu
     # self$D = ncol(self$eta)
   },
-  forward = function(yphi_, Sigma, mu, by_batch = TRUE,b){
+  forward = function(yphi_, Sigma, mu, by_batch = TRUE){
     # SigmaInv = Sigma$inverse()
+    if(by_batch){
       D = ncol(self$eta)
       ## This is an option to calculate the loss function that is slower, but more memory efficient. 
       ## When D > 100 this option is recommended. 
       ## Ideadly we could change the step sizes by chunks of at most 50. 
+      batches = msplit(1:ncol(self$eta), ceiling(ncol(self$eta)/50))
       fun2 = 0
       SigmaInv = Sigma$inverse()
-    if(length(b) == 1){
-        theta = nnf_softmax(torch_cat(c(self$eta[,b, drop=FALSE], torch_zeros(1,1, device = device)), dim=1), dim=1)
-        diff1 = self$eta[,d, drop=FALSE]- mu[,b, drop=FALSE]
-        fun = -0.5*diff1$transpose(1,2)$matmul(SigmaInv)$matmul(diff1)
-        fun1 = fun + yphi_[,,,,,b,,]$matmul(torch_log(theta+1e-14))$sum()
-        fun2 =  fun2 -fun1
+      for (b in batches){
+        if(length(b) == 1){
+          theta = nnf_softmax(torch_cat(c(self$eta[,b, drop=FALSE], torch_zeros(1,1, device = device)), dim=1), dim=1)
+          diff1 = self$eta[,d, drop=FALSE]- mu[,b, drop=FALSE]
+          fun = -0.5*diff1$transpose(1,2)$matmul(SigmaInv)$matmul(diff1)
+          fun1 = fun + yphi_[,,,,,b,,]$matmul(torch_log(theta+1e-14))$sum()
+          fun2 =  fun2 -fun1
+        } else {
+          theta = nnf_softmax(torch_cat(c(self$eta[,b,drop=FALSE], torch_zeros(1, length(b), device = device)), dim=1), dim=1)
+          diff1 = self$eta[,b, drop=FALSE] - mu[,b, drop=FALSE]
+          fun = torch_diag(-0.5*diff1$transpose(1,2)$matmul(SigmaInv)$matmul(diff1))
+          fun1 = fun + torch_diag(yphi_[,,,,,b,,]$matmul(torch_log(theta+1e-14))$sum(dim=c(1,2,3,4,5,7)))
+          fun2 = fun2 -fun1$mean()
+        }
+      }
     } else {
-        theta = nnf_softmax(torch_cat(c(self$eta[,b,drop=FALSE], torch_zeros(1, length(b), device = device)), dim=1), dim=1)
-        diff1 = self$eta[,b, drop=FALSE] - mu[,b, drop=FALSE]
-        fun = torch_diag(-0.5*diff1$transpose(1,2)$matmul(SigmaInv)$matmul(diff1))
-        fun1 = fun + torch_diag(yphi_[,,,,,b,,]$matmul(torch_log(theta+1e-14))$sum(dim=c(1,2,3,4,5,7)))
-        fun2 = fun2 -fun1$mean()
+      theta = nnf_softmax(torch_cat(c(self$eta, torch_zeros(1, D, device = device)), dim=1), dim=1)
+      diff1 = self$eta - self$mu
+      fun = torch_diag(-0.5*diff1$transpose(1,2)$matmul(SigmaInv)$matmul(diff1))
+      fun1 = fun + torch_diag(yphi_$matmul(torch_log(theta + 1e-14))$sum(dim=c(1,2,3,4,5,7)))
+      fun2 = -fun1$mean()
     }
     return(fun2)
   } 
@@ -51,20 +62,16 @@ update_eta_Delta <- function(T0, covs, eta, Sigma, Y,Xi, X, hyp){
   yphi_ = yphi(covs=covs, T0 = T0, Y= Y, missing_rate = make_m__(Y), X = X, context=TRUE,eta = eta$clone())
   tmp_mod = estimate_theta(eta)
   optimizer = optim_adam(tmp_mod$parameters, lr = lr)
-  batches = msplit(1:ncol(eta), ceiling(ncol(eta)/50))
-  total_loss = 0
   while (converged == FALSE && it <= max_iter){
     it = it+1
     optimizer$zero_grad()
-    for (b in batches){
-        new_loss = tmp_mod(yphi_, Sigma, mu,by_batch=TRUE,b)
-        new_loss$backward()
-        total_loss = total_loss + new_loss$item()
-    }
-    ## moved optimizer step outside the loop
+    new_loss = tmp_mod(yphi_, Sigma, mu,by_batch=TRUE)
+
+   # current implementation is memory intensive, need to call gc()
+    new_loss$backward()
     optimizer$step() 
-    converged = theta_stop(total_loss, old_loss, tol)
-    old_loss = total_loss
+    converged = theta_stop(new_loss$item(), old_loss, tol)
+    old_loss = new_loss$item()
     gc()
   }
   eta= tmp_mod$parameters$eta$detach()
