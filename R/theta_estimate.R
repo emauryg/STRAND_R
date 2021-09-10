32b7df1c728386c00ee568e91c1d304f6d698a95
emauryg <emauryg1@gmail.com>
2021-09-10 08:25:07 -0400

    update

--- R/theta_estimate.R
+++ R/theta_estimate.R
@@ -24,7 +24,7 @@ estimate_theta <- torch::nn_module(
         yphi_ = yphi(covs=covs, T0 = T0, Y= Y[..,b,drop=FALSE], missing_rate = make_m__(Y[..,b,drop=FALSE]), X = X[b,,drop=FALSE], context=TRUE,eta = self$eta[,b,drop=FALSE])
         if(length(b) == 1){
           theta = nnf_softmax(torch_cat(c(self$eta[,b, drop=FALSE], torch_zeros(1,1, device = device)), dim=1), dim=1)
-          diff1 = self$eta[,d, drop=FALSE]- mu[,b, drop=FALSE]
+          diff1 = self$eta[,b, drop=FALSE]- mu[,b, drop=FALSE]
           fun1 = -0.5*diff1$transpose(1,2)$matmul(SigmaInv)$matmul(diff1) + yphi_$matmul(torch_log(theta+1e-14))$sum()
           fun2 =  fun2 - fun1
           rm(theta); rm(diff1); rm(fun1); rm(yphi_)
