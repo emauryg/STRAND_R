44a0b2e1ea57d6be5f164a7a17c7f14b28088387
emauryg <emauryg1@gmail.com>
2021-09-10 02:07:04 -0400

    updating memory handling

--- R/theta_estimate.R
+++ R/theta_estimate.R
@@ -26,12 +26,14 @@ estimate_theta <- torch::nn_module(
           diff1 = self$eta[,d, drop=FALSE]- mu[,b, drop=FALSE]
           fun1 = -0.5*diff1$transpose(1,2)$matmul(SigmaInv)$matmul(diff1) + yphi_$matmul(torch_log(theta+1e-14))$sum()
           fun2 =  fun2 - fun1
+          rm(theta); rm(diff1); rm(fun1); rm(yphi_)
           gc()
         } else {
           theta = nnf_softmax(torch_cat(c(self$eta[,b,drop=FALSE], torch_zeros(1, length(b), device = device)), dim=1), dim=1)
           diff1 = self$eta[,b, drop=FALSE] - mu[,b, drop=FALSE]
           fun1 = torch_diag(-0.5*diff1$transpose(1,2)$matmul(SigmaInv)$matmul(diff1)) + torch_diag(yphi_$matmul(torch_log(theta+1e-14))$sum(dim=c(1,2,3,4,5,7)))
           fun2 = fun2 -fun1$mean()
+          rm(theta); rm(diff1); rm(fun1); rm(yphi_)
           gc()
         }
       }
