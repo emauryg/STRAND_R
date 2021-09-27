09e85758f5710a8714da75fc7a1de476b7f4cbd3
Edauardo Maury <Eduardo_Maury@hms.harvard.edu>
2021-09-27 10:56:42 -0400

    update enc_start_func

--- R/tnf_batch2.R
+++ R/tnf_batch2.R
@@ -78,7 +78,9 @@ enc_start_func <- function(Y,phi){
     yphi_sum_e = 0
     yphi_sum_n = 0
     yphi_sum_c = 0
-    for (d in 1:D){
+    batch_size=128
+    batch_idx = msplit(1:D, floor(D/batch_size))
+    for (d in batch_idx){
         yphi_sum_e = yphi_sum_e + (Y[..,d,,]*phi[..,d,,])$sum(dim=c(1,2,4,5,-2))
         yphi_sum_n = yphi_sum_n + (Y[..,d,,]*phi[..,d,,])$sum(dim=c(1,2,3,5,-2))
         yphi_sum_c = yphi_sum_c + (Y[..,d,,]*phi[..,d,,])$sum(dim=c(1,2,3,4,-2))
