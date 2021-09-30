4c88826d91bb9116b5d4da82bf6519070436f882
Edauardo Maury <Eduardo_Maury@hms.harvard.edu>
2021-09-30 17:56:35 -0400

    update

--- R/bash_runs/run_strand.sh
+++ R/bash_runs/run_strand.sh
@@ -13,12 +13,12 @@ X=${2}
 output_dir=${3}
 init_path=${4}
 
-# sbatch -t 12:00:00 --mem=40G -n 1 -c 4 -p gpu --gres=gpu:${GPU_TYPE}:${NUMBER_OF_GPU} \
-#     --wrap="LD_PRELOAD=/n/data1/bch/genetics/lee/eam63/projects/admixsig/glibc-2.31/lib/libm.so.6 Rscript \
-#         main_strand.R -c ${count_tensor} -x ${X} -k 20 --tau 1 -o ${output_dir} --init_path ${4}"
-
-
-sbatch -t 12:00:00 --mem=70G -n 1 -c 4 -p short \
+sbatch -t 12:00:00 --mem=60G -n 1 -c 4 -p gpu --gres=gpu:${GPU_TYPE}:${NUMBER_OF_GPU} \
     --wrap="LD_PRELOAD=/n/data1/bch/genetics/lee/eam63/projects/admixsig/glibc-2.31/lib/libm.so.6 Rscript \
         main_strand.R -c ${count_tensor} -x ${X} -k 20 --tau 1 -o ${output_dir} --init_path ${4}"
+
+
+# sbatch -t 12:00:00 --mem=70G -n 1 -c 4 -p short \
+#     --wrap="LD_PRELOAD=/n/data1/bch/genetics/lee/eam63/projects/admixsig/glibc-2.31/lib/libm.so.6 Rscript \
+#         main_strand.R -c ${count_tensor} -x ${X} -k 20 --tau 1 -o ${output_dir} --init_path ${4}"
 
