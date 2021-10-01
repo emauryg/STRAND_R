c5812fb2019a1c43acbb0528915ee02982080ae4
Edauardo Maury <Eduardo_Maury@hms.harvard.edu>
2021-09-30 22:46:12 -0400

    update

--- R/tnf_batch2.R
+++ R/tnf_batch2.R
@@ -123,15 +123,13 @@ stop_run <- function(old_loss_, loss,tol, cur_patience){
 
 
 tnf_fit <- function(factors, T0,Y, tau,eta, device0){
-    make_m__(Y)
-    T_tensor = stack(T0=T0, bt = factors$bt, br = factors$br)
-    F_tensor = factors_to_F(factors=factors, missing_rate =  m_)
+    m_ = make_m__(Y)
+    T_tensor = stack(T0=T0, bt = factors$bt, br = factors$br, do_gpu=FALSE)
+    F_tensor = factors_to_F(factors=factors, missing_rate =  m_, do_gpu=FALSE)
     #phi = Phi(eta, T_tensor, F_tensor)
     
-    print("hello")
 
     D = Y$size(dim=-1)
-    K = phi$size(dim=-1)
     Y = Y$transpose(-1, -2)$unsqueeze(-1)
 
     # split train and validation set
@@ -141,7 +139,7 @@ tnf_fit <- function(factors, T0,Y, tau,eta, device0){
     Y_train = Y[,,,,,torch_tensor(as.integer(train_index)),,]
     
     phi_train = Phi(eta[,train_index], T_tensor, F_tensor, to_gpu=FALSE)
-    print("hello2")
+    K = phi$size(dim=-1)
     Y_valid = Y[,,,,,torch_tensor(as.integer(valid_index)),,]
     phi_valid = Phi(eta[,valid_index], T_tensor, F_tensor, to_gpu=FALSE)
     # yphi_valid = torch_empty(c(3,3,16,4,2,96,K), device=device)
